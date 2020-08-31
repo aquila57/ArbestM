698abcd2a37c460bd8513ad4c0cf7042e29c5d25
aquila57 <58740691+aquila57@users.noreply.github.com>
2020-08-30 21:48:10 -0500

    Add files via upload

---
+++ UseThree.java
@@ -0,0 +1,33 @@
+/**************************************************************************
+ *  Compilation:  javac UseArgument.java
+ *  Execution:    java UseArgument yourname
+ *
+ *  Prints "Hi, Bob. How are you?" where "Bob" is replaced by the
+ *  command-line argument.
+ *
+ *  % java UseArgument Bob
+ *  Hi, Bob. How are you?
+ *
+ *  % java UseArgument Alice
+ *  Hi, Alice. How are you?
+ *
+ 
+**************************************************************************/
+/* Copyright © 2000–2017, Robert Sedgewick and Kevin Wayne.
+Last updated: Fri Oct 20 14:12:12 EDT 2017. */
+
+public class HiName {
+
+    public static void main(String[] args) {
+        System.out.print("Hi, ");
+        System.out.print(args[2]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(", ");
+        System.out.print(args[0]);
+        System.out.println(". How are you?");
+    }
+
+}
+
+
