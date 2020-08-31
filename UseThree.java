fe2f36f32dd9b0f888d6fa7d1e80b2521272b11e
aquila57 <58740691+aquila57@users.noreply.github.com>
2020-08-30 21:55:19 -0500

    Update UseThree.java

--- UseThree.java
+++ UseThree.java
@@ -16,7 +16,7 @@
 /* Copyright © 2000–2017, Robert Sedgewick and Kevin Wayne.
 Last updated: Fri Oct 20 14:12:12 EDT 2017. */
 
-public class HiName {
+public class UseThree {
 
     public static void main(String[] args) {
         System.out.print("Hi, ");
