b68f64556370713c3d49dbe08a2d1e6102e64e8c
aquila57 <58740691+aquila57@users.noreply.github.com>
2020-08-30 20:53:28 -0500

    Add files via upload

---
+++ Initials.java
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *  Compilation:  javac Initials.java
+ *  Execution:    java Initials
+ *
+ *  Prints my initials in a font of asterisks
+ *
+ *  % java Initials
+ *  (initials in a font of asterisks)
+ *
+ **************************************************************************/
+/* Copyright © 2020,  aquila57 at github.com  */
+
+public class Initials {
+
+    public static void main(String[] args) {
+        // Prints initials to the terminal window.
+        System.out.println("");
+        System.out.println("*******        ******      ******");
+        System.out.println("*      *      *           *");
+        System.out.println("*       *    *           *");
+        System.out.println("*      *    *             *");
+        System.out.println("*******     *              *****");
+        System.out.println("* *         *     ***           *");
+        System.out.println("*   *        *      *            *");
+        System.out.println("*     *       *     *           *");
+        System.out.println("*       *      ******     ******");
+        System.out.println("");
+    } // public static void main(String[] args)
+
+} // Initials class
